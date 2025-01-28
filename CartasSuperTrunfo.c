e51f4cf397b12cb9b38f613c0618d33072365f73
DanielAtos-20-24 <danielmoreicustodio1999@gmail.com>
2025-01-28 02:33:58 +0000

    Criado o programa jogo super trunfo.

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -2,21 +2,42 @@
 
 // Desafio Super Trunfo - Países
 // Tema 1 - Cadastro das Cartas
-// Este código inicial serve como base para o desenvolvimento do sistema de cadastro de cartas de cidades.
-// Siga os comentários para implementar cada parte do desafio.
-//Teste larissa
+//Teste Daniel Moreira
 
 int main() {
-    // Sugestão: Defina variáveis separadas para cada atributo da cidade.
-    // Exemplos de atributos: código da cidade, nome, população, área, PIB, número de pontos turísticos.
-    
-    // Cadastro das Cartas:
-    // Sugestão: Utilize a função scanf para capturar as entradas do usuário para cada atributo.
-    // Solicite ao usuário que insira as informações de cada cidade, como o código, nome, população, área, etc.
-    
-    // Exibição dos Dados das Cartas:
-    // Sugestão: Utilize a função printf para exibir as informações das cartas cadastradas de forma clara e organizada.
-    // Exiba os valores inseridos para cada atributo da cidade, um por linha.
 
-    return 0;
+    int codigo;
+    char nome[20];
+    int populacao;
+    float area;
+    float pib;
+    char pontosTuristicos [100];
+    
+    printf("Digite o código da cidade: \n");
+    scanf("%d", &codigo);
+
+    printf("Digite o nome da cidade:\n");
+    scanf("%s", &nome);
+
+    printf("Digite a população: \n");
+    scanf("%d", &populacao);
+
+    printf("Digite a área da cidade: \n"); 
+    scanf("%f", &area);
+
+    printf("Digite o PIB: \n");
+    scanf("%f", &pib);
+
+    printf("Digite os pontos turisticos: \n");
+    scanf("%s", &pontosTuristicos);
+
+    printf("Código da cidade: %d\n", codigo);
+    printf("Nome da cidade: %s\n", nome);
+    printf("População: %d\n" , populacao);
+    printf("Área da cidade: %f\n", area);
+    printf("PIB da cidade: %f\n", pib);
+    printf("Pontos turisticos: %s\n", pontosTuristicos);
+
+ return 0;
 }
+
